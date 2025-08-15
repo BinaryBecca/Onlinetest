3b4b5a62977fb9210a91b44150c590d768b45c77
BinaryBecca <rebecca.thinnes@web.de>
2025-08-15 11:50:10 +0200

    github

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   // für github
-  // base: "/onlinetest/",
+  base: "/onlinetest/",
   // für netlify
-  base: "/",
+  // base: "/",
 })
