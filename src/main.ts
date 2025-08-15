f8bbe7be309dff67e69b442e2d257ba856da21c8
BinaryBecca <rebecca.thinnes@web.de>
2025-08-15 12:27:19 +0200

    123

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,5 @@ const hiBtn = document.getElementById("hiBtn") as HTMLButtonElement
 const divInnerHTML = document.getElementById("divInnerHTML") as HTMLDivElement
 
 hiBtn.addEventListener("click", () => {
-  hiBtn.innerHTML = ""
-  divInnerHTML.innerHTML = ""
-
   divInnerHTML.innerHTML = "Hi"
 })
